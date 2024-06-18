596695d8be066ca14c76721b019419538526825d
nminaian <n.minaian@gmail.com>
2024-06-18 12:52:22 -0700

    editing measure_power

--- src/keithley_daq/measure_power.py
+++ src/keithley_daq/measure_power.py
@@ -39,22 +39,22 @@ with get_instrument() as inst:
 
         # define the scan list, set scan count to infinite, and a channel delay of 100 us
         inst.write(":ROUT:SCAN:CRE (@101:103)")  # Generate the scan...
-        # inst.write(":ROUT:SCAN:COUN:SCAN 0")
+        inst.write(":ROUT:SCAN:COUN:SCAN 0")
 
         # enable the graph and plot the data
         inst.write(":DISP:SCR HOME")
         inst.write(":DISP:WATC:CHAN (@101:103)")
         inst.write(":DISP:SCR GRAP")
-        inst.write("ROUT:CHAN:LAB 'IPMC 1', (@101)")  # Apply a label to channel 101
-        inst.write("ROUT:CHAN:LAB 'IPMC 2', (@102)")  # Apply a label to channel 102
-        inst.write("ROUT:CHAN:LAB 'IPMC 3', (@103)")  # Apply a label to channel 103
+        inst.write("ROUT:CHAN:LAB 'IPMC1', (@101)")  # Apply a label to channel 101
+        inst.write("ROUT:CHAN:LAB 'IPMC2', (@102)")  # Apply a label to channel 102
+        inst.write("ROUT:CHAN:LAB 'IPMC3', (@103)")  # Apply a label to channel 103
         inst.write(
             "SENS:FUNC 'VOLT:DC:RAT', (@101:103)"
         )  # Set channels 101-103 for DC Voltage Ratio
 
         # begin data collection for at least xx sec
         inst.write("INIT")
-        time.sleep(50)
+        time.sleep(18)
 
     except KeyboardInterrupt:
         print("Measurement stopped by user. \n")
@@ -65,27 +65,31 @@ with get_instrument() as inst:
 
     buffersize = inst.query(':TRAC:ACTual:END? "Power"')
     print(buffersize)
-    ass = inst.query(f'TRAC:DATA? 1, {buffersize}, "Power", READ, EXTR').split(",")
+    ass = inst.query(f'TRAC:DATA? 1, {buffersize}, "Power", READ, EXTR, REL').split(",")
     buffer = [float(val) for val in ass]
+
     # Data = pd.DataFrame({'Voltage':buffer[0::3], 'Time':buffer[2::3], 'Channel':buffer[1::3]}).to_csv('Butt.csv')
     SHUNT = 10.3
     Data = pd.DataFrame({
-        "ratio1": buffer[::6],
-        "vsense1": buffer[1::6],
-        "ratio2": buffer[2::6],
-        "vsense2": buffer[3::6],
-        "ratio3": buffer[4::6],
-        "vsense3": buffer[5::6],
+        "ratio1": buffer[::9],
+        "vsense1": buffer[1::9],
+        "time1": buffer[2::9],
+        "ratio2": buffer[3::9],
+        "vsense2": buffer[4::9],
+        "time2": buffer[5::9],
+        "ratio3": buffer[6::9],
+        "vsense3": buffer[7::9],
+        "time3": buffer[8::9],
     }).assign(**{
         "Current 1 [A]": lambda df: df.vsense1 / SHUNT,
         "Voltage 1 [V]": lambda df: df.ratio1 * df.vsense1,
-        "Power 1 [W]": lambda df: df["Current [mA]"] * df["Voltage [mV]"],
+        "Power 1 [W]": lambda df: df["Current 1 [A]"] * df["Voltage 1 [V]"],
         "Current 2 [A]": lambda df: df.vsense2 / SHUNT,
         "Voltage 2 [V]": lambda df: df.ratio2 * df.vsense2,
-        "Power 2 [W]": lambda df: df["Current [mA]"] * df["Voltage [mV]"],
+        "Power 2 [W]": lambda df: df["Current 2 [A]"] * df["Voltage 2 [V]"],
         "Current 3 [A]": lambda df: df.vsense3 / SHUNT,
         "Voltage 3 [V]": lambda df: df.ratio3 * df.vsense3,
-        "Power 3 [W]": lambda df: df["Current [mA]"] * df["Voltage [mV]"],
+        "Power 3 [W]": lambda df: df["Current 3 [A]"] * df["Voltage 3 [V]"],
     })
 
     Data.to_csv("Data.csv")
